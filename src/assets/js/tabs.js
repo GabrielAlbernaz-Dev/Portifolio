0534d320a24c3418f57519315416fbd0058c17d9
Gabriel Albernaz <gabrielalbernaz22@gmail.com>
2022-09-08 03:13:05 -0300

    Experience section changes

--- src/assets/js/tabs.js
+++ src/assets/js/tabs.js
@@ -1,25 +1,27 @@
-const tabs = document.querySelectorAll(".tabs li")
-const contentTabs = document.querySelectorAll(".content-tabs .content")
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll(".tabs li")
+    const contentTabs = document.querySelectorAll(".content-tabs .content")
 
-function initTabs(tab,content) {
-    tab[0].classList.add("active")
-    content[0].classList.add("active")
-}
-initTabs(tabs,contentTabs)
+    function initTabs(tab, content) {
+        tab[0].classList.add("active")
+        content[0].classList.add("active")
+    }
+    initTabs(tabs, contentTabs)
 
-function activeTab(index) {
-    contentTabs.forEach((contentTab) => {
-        contentTab.classList.remove("active")
-    })
-    tabs.forEach((contentTab) => {
-        contentTab.classList.remove("active")
-    })
-    contentTabs[index].classList.add("active")
-    tabs[index].classList.add("active")
-}
+    function activeTab(index) {
+        contentTabs.forEach((contentTab) => {
+            contentTab.classList.remove("active")
+        })
+        tabs.forEach((contentTab) => {
+            contentTab.classList.remove("active")
+        })
+        contentTabs[index].classList.add("active")
+        tabs[index].classList.add("active")
+    }
 
-tabs.forEach((tab,i) => {
-    tab.addEventListener("click", ()=>{
-        activeTab(i)
+    tabs.forEach((tab, i) => {
+        tab.addEventListener("click", () => {
+            activeTab(i)
+        })
     })
 })
